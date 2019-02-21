2beac503c2b0fbb59160497ae3e47e0ed8b2d8b6
Ab633014 <46719336+Ab633014@users.noreply.github.com>
2019-02-20 22:11:48 -0500

    Chapter 5 Commit/pull
    Finished assignment

---
+++ baileyUnityGitDemo/Assets/WhatHappened.cs
@@ -0,0 +1,20 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhatHappened : MonoBehaviour, IFocusable
+{
+
+        public void OnFocusEnter()
+        {
+            Debug.Log("I'm looking at a " + gameObject.name);
+        }
+        public void OnFocusExit()
+        {
+            Debug.Log("I'm no longer looking at a " + gameObject.name);
+        }
+ }
+
+ 
+
